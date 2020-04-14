3675b39e79398b51be49cf1c84939f30db191623
Charles Butler <cdbutle@outlook.com>
2020-04-14 00:40:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,12 +3,6 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
 
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
-    @id = id
-  end
-
   def self.create_table
     sql =<<-SQL
     CREATE TABLE IF NOT EXISTS dogs (
